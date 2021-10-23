4675414d3ecedc7a7fc4cca3d64a15f3bda3a3d7
Rick Brown <ricky.brown.00@gmail.com>
2021-10-23 16:05:13 +0100

    Initial Commit

---
+++ iOS_UI_Portfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_UI_Portfolio
+//
+//  Created by Rick Brown on 23/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_UI_Portfolio/iOS_UI_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_UI_PortfolioApp.swift
+//  iOS_UI_Portfolio
+//
+//  Created by Rick Brown on 23/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOS_UI_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
